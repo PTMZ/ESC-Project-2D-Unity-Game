98e4f8e258ecd8d330f955831043bb2632eaafc0
ChokHaoZe <42269203+ChokHaoZe@users.noreply.github.com>
2019-03-15 02:03:52 +0800

    (update_on_puzzleLevel)

--- Assets/Scripts/PortalToSneakyLevel.cs
+++ Assets/Scripts/PortalToSneakyLevel.cs
@@ -7,9 +7,109 @@ public class PortalToSneakyLevel : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+
+        int count = 0;
+
+        if ((other.CompareTag("Cone")))
+        {
+            count += 1;
+        }
+        if (count == 4)
         {
             SceneManager.LoadScene("SneakyLevel");
         }
     }
 }
+        /*if ((other.CompareTag("Cone")))
+
+        {
+            if ((other.CompareTag("Cone1")))
+
+            {
+                if ((other.CompareTag("Cone2")))
+
+                {
+                    SceneManager.LoadScene("SneakyLevel");
+                }
+            }
+        }
+
+        if ((other.CompareTag("Cone")))
+
+        {
+            if ((other.CompareTag("Cone2")))
+
+            {
+                if ((other.CompareTag("Cone1")))
+
+                {
+                    SceneManager.LoadScene("SneakyLevel");
+                }
+            }
+        }
+
+        if ((other.CompareTag("Cone1")))
+
+        {
+            if ((other.CompareTag("Cone0")))
+
+            {
+                if ((other.CompareTag("Cone2")))
+
+                {
+                    SceneManager.LoadScene("SneakyLevel");
+                }
+            }
+        }
+
+        if ((other.CompareTag("Cone1")))
+
+        {
+            if ((other.CompareTag("Cone2")))
+
+            {
+                if ((other.CompareTag("Cone0")))
+
+                {
+                    SceneManager.LoadScene("SneakyLevel");
+                }
+            }
+        }
+
+        if ((other.CompareTag("Cone2")))
+
+        {
+            if ((other.CompareTag("Cone0")))
+
+            {
+                if ((other.CompareTag("Cone1")))
+
+                {
+                    SceneManager.LoadScene("SneakyLevel");
+                }
+            }
+        }
+
+        if ((other.CompareTag("Cone2")))
+
+        {
+            if ((other.CompareTag("Cone1")))
+
+            {
+                if ((other.CompareTag("Cone0")))
+
+                {
+                    SceneManager.LoadScene("SneakyLevel");
+                }
+            }
+        }*/
+
+
+            /*if (other.CompareTag("Player"))
+
+            {
+                SceneManager.LoadScene("PuzzleLevel");
+            }*/
+    
+
+
