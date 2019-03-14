ee1fe5e65d84f5e1c4869a10a52136be40614f05
ChokHaoZe <42269203+ChokHaoZe@users.noreply.github.com>
2019-03-14 19:40:19 +0800

    (trigger_portal)

---
+++ Assets/Scripts/PortalToContrictLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalToContrictLevel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Untagged"))
+        {
+            SceneManager.LoadScene("ConstrictLevel");
+        }
+    }
+}
