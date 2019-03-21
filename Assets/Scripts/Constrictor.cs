8b725d21421f81e97c8dd203db32bacecfa73ad1
ChokHaoZe <42269203+ChokHaoZe@users.noreply.github.com>
2019-03-21 23:55:38 +0800

    (constrictMap_working)

---
+++ Assets/Scripts/Constrictor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constrictor : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    public float speed;//speed of the game object
+    double WPradius = 0.5;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Vector2.Distance(waypoints[current].transform.position, transform.position) < WPradius)//distance between waypoint and player within radius
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);//moving the object towards the waypoint
+        
+    }
+}
---
+++ Assets/Scripts/Level Portals/PortalForContrictLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalForContrictLevel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("PuzzleLevel");
+        }
+        if (other.CompareTag("Constrict"))
+        {
+            SceneManager.LoadScene("ConstrictLevel");
+        }
+    }
+}
