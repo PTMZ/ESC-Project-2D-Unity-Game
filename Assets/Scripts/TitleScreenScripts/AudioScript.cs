8b5cc086ad15c12f6e886d444ee912a907ad0c5d
SeanLim <kh2_rox@hotmail.com>
2019-03-22 12:39:50 +0800

    removed menu

---
+++ Assets/Scripts/TitleScreenScripts/AudioScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+    public static int startCount = 0;
+    public static int awakeCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        MusicSource.clip = MusicClip;
+
+        if (startCount == 0)
+        {
+            MusicSource.Play();
+            startCount++;
+        }
+        
+    }
+
+    void Awake()
+    {
+        //string currentScene = SceneManager.GetActiveScene().name;
+        if (awakeCount == 0)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            awakeCount++;
+        }
+    }
+}
