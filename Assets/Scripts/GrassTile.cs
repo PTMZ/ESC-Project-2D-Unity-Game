9b5d6061856a4525696d0d15454049d3790e541e
gcgloven <gong.chen.ccstudio@gmail.com>
2019-03-29 14:20:37 +0800

    Grass Save

---
+++ Assets/Scripts/GrassTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class GrassTile : Tile
+{
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/GrassTile")]
+    public static void CreateGrassTile() {
+        string path = EditorUtility.SaveFilePanelInProject("Save GrassTile", "GrassTile", "asset", "Save GrassTile", "Assets");
+        if (path == "") {
+            return;
+        }
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<GrassTile>(), path);
+    }
+}
+#endif
