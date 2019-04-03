7b29baf3d88a573b0d71e8d500581ff0d65bc179
ChokHaoZe <42269203+ChokHaoZe@users.noreply.github.com>
2019-04-03 18:05:26 +0800

    (mineswepermap)

--- Assets/Scripts/MinesweeperGrass.cs
+++ Assets/Scripts/MinesweeperGrass.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class MinesweeperGrass : MonoBehaviour
 {
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
+    }
+
+    void LateUpdate()
+    {
+        mySpriteRenderer.sortingOrder = (int)Camera.main.WorldToScreenPoint(mySpriteRenderer.bounds.min).y * -1;
     }
 }
