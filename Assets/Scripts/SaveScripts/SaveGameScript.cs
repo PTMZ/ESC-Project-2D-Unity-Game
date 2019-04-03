f074051e92a7c974a1be5ff295c3071b281dde81
PTMZ <paultmz88@gmail.com>
2019-04-03 16:22:45 +0800

    Removed portal scripts, added save game scripts, incomplete

---
+++ Assets/Scripts/SaveScripts/SaveGameScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveGame()
+    {
+        // 1
+        Save save = CreateSaveGameObject();
+
+        // 2
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        // 3
+        // Reset whatever is needed
+
+        Debug.Log("Game Saved");
+    }
+
+    private Save CreateSaveGameObject(){
+        Save save = new Save();
+
+        save.health = FindObjectOfType<PlayerAvatar>().health;
+        save.curAttack = OfflineGameManager.instance.curAttack;
+        save.curScene = SceneManager.GetActiveScene().name;
+
+        return save;
+    }
+
+    public void LoadGame()
+    { 
+        // 1
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            // 2
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+
+            // Update Game state
+            Debug.Log("Game Loaded");
+        }
+        else
+        {
+            Debug.Log("No game saved!");
+        }
+    }
+}
