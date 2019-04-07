80575d384e195dab402fe4b2f1d99469b0a66c9f
PTMZ <paultmz88@gmail.com>
2019-04-07 23:02:49 +0800

    Finsished B3

--- Assets/Scripts/BoxButtonScripts/BoxButton.cs
+++ Assets/Scripts/BoxButtonScripts/BoxButton.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class BoxButton : MonoBehaviour
 {
     //public ButtonTrail buttonTrail;
+    public Sprite normSprite;
+    public Sprite pressSprite;
+    public GameObject trailLock;
+    public GameObject trailSignal;
+    public GameObject tick;
+    public GameObject cross;
+    public GameObject laser;
     private int count = 0;
+    private SpriteRenderer mySpriteRenderer;
+
+    void Start(){
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        mySpriteRenderer.sprite = normSprite;
+        trailLock.SetActive(true);
+        trailSignal.SetActive(false);
+        cross.SetActive(true);
+        tick.SetActive(false);
+        laser.SetActive(true);
+    }
 
     void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("Destroyable")){
+        if(other.CompareTag("DestroyableBox")){
             count ++;
+        }
+        if(getPressed()){
+            mySpriteRenderer.sprite = pressSprite;
+            trailLock.SetActive(false);
+            trailSignal.SetActive(true);
+            cross.SetActive(false);
+            tick.SetActive(true);
+            laser.SetActive(false);
         }
     }
 
     void OnTriggerExit2D(Collider2D other){
-        if(other.CompareTag("Destroyable")){
+        if(other.CompareTag("DestroyableBox")){
             count --;
+        }
+        if(!getPressed()){
+            mySpriteRenderer.sprite = normSprite;
+            trailLock.SetActive(true);
+            trailSignal.SetActive(false);
+            cross.SetActive(true);
+            tick.SetActive(false);
+            laser.SetActive(true);
         }
     }
 
