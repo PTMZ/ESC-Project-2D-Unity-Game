0f8f94c5513b13f5ffd54b069e96c3fd89a34e5b
SeanLim <kh2_rox@hotmail.com>
2019-04-08 12:39:28 +0800

    Dialogue trigger fixed

--- Assets/Scripts/InGameUIScripts/DialogueTrigger.cs
+++ Assets/Scripts/InGameUIScripts/DialogueTrigger.cs
@@ -14,19 +14,19 @@ public class DialogueTrigger : MonoBehaviour
 
     void Start()
     {
-        storyProgress = OfflineGameManager.instance.storyProgress;
+        //storyProgress = OfflineGameManager.instance.storyProgress;
     }
 
     public void TriggerDialogue()
     {
-        //Debug.Log("Current scene is " + SceneNumber);
-        //Debug.Log("Current story is " + storyProgress);
+        Debug.Log("Current scene is " + SceneNumber);
+        Debug.Log("Current story is " + OfflineGameManager.instance.storyProgress);
         //Checks if is story dialogue
-        if((SceneNumber) == storyProgress)
+        if ((SceneNumber) == OfflineGameManager.instance.storyProgress)
         {
             FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-            storyProgress++;
-            //Debug.Log("Story Progressed.");
+            OfflineGameManager.instance.storyProgress++;
+            Debug.Log("Story Progressed. Story is now " + OfflineGameManager.instance.storyProgress);
         }
         //else //if not story dialogue
         //{
@@ -48,6 +48,7 @@ public class DialogueTrigger : MonoBehaviour
             {
                 return;
             }
+            Debug.Log("Triggering Dialogue " + SceneNumber);
             TriggerDialogue();
         }
 
