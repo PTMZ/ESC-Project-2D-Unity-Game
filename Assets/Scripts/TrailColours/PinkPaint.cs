21c46d862c9729d653344250cc13bbaf78ac2703
ChokHaoZe <42269203+ChokHaoZe@users.noreply.github.com>
2019-04-09 04:16:16 +0800

    (paintworld_finetuned)

--- Assets/Scripts/TrailColours/PinkPaint.cs
+++ Assets/Scripts/TrailColours/PinkPaint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PinkPaint : MonoBehaviour
 {
-    void OnTriggerExit2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
