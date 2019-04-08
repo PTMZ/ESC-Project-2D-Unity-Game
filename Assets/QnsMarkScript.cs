1c374a64557aa53d0e7c5c344fc3619d15a12e6e
SeanLim <kh2_rox@hotmail.com>
2019-04-08 16:21:10 +0800

    Exclaimation mark anim state diagram edfixed.

---
+++ Assets/QnsMarkScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QnsMarkScript : MonoBehaviour
+{
+    private Animator animator;
+    public static bool isAnimating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (!isAnimating)
+        {
+            //animator.SetBool("setQnMark", true);
+            isAnimating = false;
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animator.SetBool("setQnMark", false);
+        //if (!isAnimating)
+        //{
+        //    animator.SetBool("setQnMark", true);
+        //    isAnimating = false;
+        //}
+        //else if (isAnimating)
+        //{
+        //    animator.SetBool("setQnMark", false);
+        //    isAnimating = false;
+        //}
+
+    }
+}
