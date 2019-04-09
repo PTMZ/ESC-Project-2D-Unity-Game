dc374d387a53472f04f7a0dc7f7b1095e74903e7
SeanLim <kh2_rox@hotmail.com>
2019-04-09 16:28:13 +0800

    B5 fully playable now

---
+++ Assets/Dialogue2Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue2Script : MonoBehaviour
+{
+    private bool keySpawned = false;
+    private int sceneNum = 3;
+    public GameObject key;
+
+    void FixedUpdate()
+    {
+        if (OfflineGameManager.instance.storyProgress == sceneNum && !keySpawned)
+        {
+            key.SetActive(true);
+            keySpawned = true;
+        }
+    }
+}
