3553d0f35650a0f3aa74f74cf660546f3eaef0f6
PTMZ <paultmz88@gmail.com>
2019-04-18 02:08:56 +0800

    changed colour box + button code

---
+++ Assets/Scripts/TrailColours/GeneralPaint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralPaint : MonoBehaviour
+{
+    public int colourNum;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerAvatar>().updateTrail(colourNum);
+        }
+
+        if(other.CompareTag("DestroyableBox")){
+            Debug.Log("Paint Box");
+            other.gameObject.GetComponent<Destroyable>().changeColour(colourNum);
+        }
+    }
+}
+
