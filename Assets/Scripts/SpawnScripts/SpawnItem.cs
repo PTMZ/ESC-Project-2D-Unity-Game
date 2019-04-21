e9691baf6fd12b43e86d8ad607d2b348392c49d6
SeanLim <kh2_rox@hotmail.com>
2019-04-22 02:48:59 +0800

    brought back floating text original size prefab

--- Assets/Scripts/SpawnScripts/SpawnItem.cs
+++ Assets/Scripts/SpawnScripts/SpawnItem.cs
@@ -8,20 +8,41 @@ public class SpawnItem : MonoBehaviour
     public GameObject destructiblePrefab;
     public GameObject destructible;
 
-
+    //private IEnumerator coroutine;
 
     void spawnDestructible()
     {
         //Debug.Log("Destructible spawn method called.");
-            //Debug.Log("No player object");
-            destructible = Instantiate(destructiblePrefab, Vector3.zero, Quaternion.identity);
-            destructible.transform.position = transform.position;  
+        //Debug.Log("No player object");
+        destructible = Instantiate(destructiblePrefab, Vector3.zero, Quaternion.identity);
+        destructible.transform.position = transform.position;
+        //coroutine = DelaySeconds(1.5f);
+        //StartCoroutine(coroutine);
     }
+
+    //private int spawnCount;
+    //IEnumerator DelaySeconds(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    if(destructible == null)
+    //    {
+
+    //        spawnCount++;
+    //    }
+        
+
+
+    //}
 
     void Update()
     {
         if (destructible == null)
             spawnDestructible();
+
+        //if(spawnCount >=1)
+        //{
+        //    StopCoroutine(coroutine);
+        //}
     }
 
 }
