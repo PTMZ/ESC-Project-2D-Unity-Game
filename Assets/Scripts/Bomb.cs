bd713d9f287529d89c654ba95173cc3a84315c65
ChokHaoZe <42269203+ChokHaoZe@users.noreply.github.com>
2019-04-21 17:02:13 +0800

    (bomb)

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -8,12 +8,15 @@ public class Bomb : MonoBehaviour
     public GameObject bomb;
 
     //public float explosionDelay = 1f;
+    //public float explosionMaxSize = 1f;
+    //public float explosionSpeed = 10f;
+    //public float explosionRate = 1f;
+    //public float currentRadius = 0.1f;
     public float bombDamage = 100;
-    public float explosionRate = 1f;
-    public float explosionMaxSize = 1f;
-    public float explosionSpeed = 10f;
-    public float explosionForce = 10f;
-    public float currentRadius = 0.1f;
+    public float impactRadius = 2;
+    public float impactPower = 5;
+    public float explosionForce = 1000f;
+    public float expRadius = 10f;       
     public Animator animator;
 
     bool exploded = false;
@@ -26,6 +29,7 @@ public class Bomb : MonoBehaviour
 
     }
 
+    /*
     void FixedUpdate()
     {
         if (exploded == true)
@@ -41,47 +45,68 @@ public class Bomb : MonoBehaviour
             explosionRadius.edgeRadius = currentRadius;
         }
     }
+    */
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject.name);
-        if (collision.CompareTag("Player"))
-        {
-            exploded = true;
-            animator.Play("bomb_explosion", -1);
-        }
-
-        
-        if (collision.CompareTag("Enemy"))
-        {
-            //exploded = false;
-            //Debug.Log("HIT ENEMY");
-            //animator.Play("bomb_explosion", -1);
-        }
         
         if (collision.gameObject.GetComponent<PlayerAvatar>() != null)
         {
+            exploded = true;
+            animator.Play("bomb_explosion", -1);
             collision.gameObject.GetComponent<PlayerAvatar>().getHit(bombDamage);
-
+            Destroy(gameObject, 0.3f);
         }
 
         if (collision.gameObject.GetComponent<EnemyAvatar>() != null)
         {
             collision.gameObject.GetComponent<EnemyAvatar>().getHit(25);
             animator.Play("bomb_explosion", -1);
-            //offlineGM.destroyBomb(gameObject);
-            Destroy(gameObject, 0.2f);
-
+            Destroy(gameObject, 0.3f);
         }
         if (collision.gameObject.GetComponent<Rigidbody2D>() != null && exploded == true)
         {
+
+            /*
             Vector2 target = collision.gameObject.transform.position;
             Vector2 bomb = gameObject.transform.position;
-            Vector2 direction1 =  explosionForce * (target - bomb);
+            Vector2 dir =  explosionForce * (target - bomb);
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(direction1);
+
+            float calc = 1 - (dir.magnitude / expRadius);
+            if (calc <= 0)
+            {
+                calc = 0;
+            }
+
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(dir.normalized * explosionForce * calc);
+            */
+
+
+            Rigidbody2D other = collision.gameObject.GetComponent<Rigidbody2D>();
+            AddExplosionForce(other, impactPower, gameObject.transform.position, impactRadius);
+            //Destroy(gameObject);
+            GameObject exp = Instantiate(bomb, transform.position, transform.rotation);
+            Destroy(exp, 0.3f);
         }
 
     }
+
+    
+        public static void AddExplosionForce(Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
+    {
+        var dir = (body.transform.position - expPosition);
+        float calc = 1 - (dir.magnitude / expRadius);
+        if (calc <= 0)
+        {
+            calc = 0;
+        }
+        body.AddForce(dir.normalized * expForce * calc);
+    }
+    
+
+
 
 
 }
---
+++ Assets/Scripts/Bomb2.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb2 : MonoBehaviour
+{
+    public float impactRadius = 2;
+    public float impactPower = 5;
+        public GameObject explosion;
+    public Animator animator;
+    public float bombDamage = 100;
+    bool exploded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.CompareTag("Player"))
+        {
+            exploded = true;
+            animator.Play("bomb_explosion", -1);
+        }
+
+
+        if (collision.CompareTag("Enemy"))
+        {
+            //exploded = false;
+            //Debug.Log("HIT ENEMY");
+            //animator.Play("bomb_explosion", -1);
+        }
+
+        if (collision.gameObject.GetComponent<PlayerAvatar>() != null)
+        {
+            collision.gameObject.GetComponent<PlayerAvatar>().getHit(bombDamage);
+
+        }
+
+        if (collision.gameObject.GetComponent<EnemyAvatar>() != null)
+        {
+            collision.gameObject.GetComponent<EnemyAvatar>().getHit(25);
+            animator.Play("bomb_explosion", -1);
+            //offlineGM.destroyBomb(gameObject);
+            Destroy(gameObject, 0.2f);
+
+        }
+        /*
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null && exploded == true)
+        {
+            Vector2 target = collision.gameObject.transform.position;
+            Vector2 bomb = gameObject.transform.position;
+            Vector2 direction1 = explosionForce * (target - bomb);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(direction1);
+        }
+        */
+
+        //Vector2 hitPoint = collision.GetContact(0).point;
+        //Rigidbody2D other = collision.otherRigidbody;
+       //AddExplosionForce(other, impactPower, new Vector3(hitPoint.x, hitPoint.y, 0), impactRadius);
+        //Destroy(gameObject);
+        GameObject exp = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(exp, 0.3f);
+
+
+    }
+
+    public static void AddExplosionForce(Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
+    {
+        var dir = (body.transform.position - expPosition);
+        float calc = 1 - (dir.magnitude / expRadius);
+        if (calc <= 0)
+        {
+            calc = 0;
+        }
+
+        body.AddForce(dir.normalized * expForce * calc);
+    }
+
+}
