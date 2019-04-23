0eb2a9632bba8d87d126bd7b5a763287c583bc7c
SeanLim <kh2_rox@hotmail.com>
2019-04-23 18:06:34 +0800

    PW04 90% Completed

--- Assets/Scripts/PuzzleScripts/PiecePuzzleManager.cs
+++ Assets/Scripts/PuzzleScripts/PiecePuzzleManager.cs
@@ -10,14 +10,14 @@ public class PiecePuzzleManager : MonoBehaviour
     public GameObject toDeactivate;
 
 
-    public int puzzleSolveCount;
+    private int puzzleSolveCount;
 
     void LateUpdate()
     {
         if (PuzzlePieces.Length == PuzzleSlots.Length)
             for (int i = 0; i < PuzzleSlots.Length; i++)
             {
-                if (PuzzleSlots[i].GetComponent<Collider2D>().IsTouching(PuzzlePieces[i].GetComponent<Pol>()))
+                if (PuzzleSlots[i].GetComponent<Collider2D>().IsTouching(PuzzlePieces[i].GetComponent<Collider2D>()))
                     puzzleSolveCount++;
             }
 
